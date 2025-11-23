e7a22eafe02be53825ed4d198423bf88f21dd018
LovishGoyal01 <goyallovish852@gmail.com>
2025-11-23 21:31:54 +0530

    editted socket cors

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -10,7 +10,7 @@ const initializeSocket = (server) => {
       
    const io = socket(server , {
       cors: {
-      origin: "http://localhost:5173",
+      origin: "*",
       methods: ["GET", "POST"],
       credentials: true
       },
